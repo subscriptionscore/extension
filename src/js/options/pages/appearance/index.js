bbeae3d641af59a48686facacea9ecb4f9e4574c
Danielle Johnson <dinkydani@gmail.com>
2019-12-10 15:31:24 +0700

    feat(theme): wire up dark mode toggle

--- src/js/options/pages/appearance/index.js
+++ src/js/options/pages/appearance/index.js
@@ -1,23 +1,89 @@
 import React, { useCallback, useContext } from 'react';
 
-import Colors from './colors';
+import { FormCheckbox } from '../../../../components/form';
 import { OptionsContext } from '../layout';
+import Rank from '../../../../components/rank';
+import styles from './appearance.module.scss';
+import { useTheme } from '../../../../utils/providers/theme-provider';
 
 const AppearancePage = () => {
-  const { state, dispatch } = useContext(OptionsContext);
-  const saveSetting = useCallback(
-    data => {
-      console.log('saving setting', data);
-      dispatch({ type: 'save-setting', data });
-    },
-    [dispatch]
-  );
   return (
     <>
       <h1>Appearance</h1>
-      <Colors settings={state.settings} onChange={saveSetting} />
+      <Colors />
     </>
   );
 };
 
 export default AppearancePage;
+
+function Colors() {
+  const { state, dispatch } = useContext(OptionsContext);
+  const { theme, toggle: toggleTheme } = useTheme();
+
+  const { colorSet, darkMode } = state.settings;
+
+  const onChangeDarkMode = useCallback(() => {
+    dispatch({ type: 'save-setting', data: { darkMode: !darkMode } });
+    toggleTheme();
+  }, [darkMode, dispatch, toggleTheme]);
+
+  return (
+    <form>
+      <div className={styles.section}>
+        <h2>Colors</h2>
+        <Radio
+          type={'normal'}
+          checked={colorSet === 'normal'}
+          onChange={() =>
+            dispatch({ type: 'save-setting', data: { colorSet: 'normal' } })
+          }
+        />
+        <Radio
+          type={'colorblind'}
+          checked={colorSet === 'colorblind'}
+          onChange={() =>
+            dispatch({ type: 'save-setting', data: { colorSet: 'colorblind' } })
+          }
+        />
+      </div>
+
+      <div className={styles.section}>
+        <div className={styles['dark-mode']}>
+          <FormCheckbox
+            name="darkMode"
+            checked={darkMode}
+            onChange={onChangeDarkMode}
+            label={<h2>Dark mode</h2>}
+          />
+        </div>
+      </div>
+    </form>
+  );
+}
+
+function Radio({ type = 'normal', checked, onChange }) {
+  return (
+    <label>
+      <input
+        type="radio"
+        name="colors-radio"
+        className={styles.radio}
+        spellCheck="false"
+        checked={checked}
+        onChange={onChange}
+      />
+      <div className={styles.ranks}>
+        <Ranks colorblind={type === 'colorblind'} />
+      </div>
+    </label>
+  );
+}
+
+function Ranks({ colorblind }) {
+  return ['A+', 'A', 'B', 'C', 'D', 'E', 'F'].map(rank => (
+    <div key={rank} className={styles.rank}>
+      <Rank rank={rank} colorblind={colorblind} />
+    </div>
+  ));
+}
