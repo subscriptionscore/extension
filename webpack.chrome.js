4ce6177c740ac501e4567c225250bf994d8a7419
James Ivings <james@squarecat.io>
2020-01-24 17:29:37 +0800

    🔼 bumped version

--- webpack.chrome.js
+++ webpack.chrome.js
@@ -9,7 +9,7 @@ const commonOptions = require('./webpack.config');
 const options = {
   mode: commonOptions.mode,
   optimization: {
-    // We no not want to minimize our code.
+    // We do not want to minimize our code.
     minimize: false
   },
   entry: commonOptions.entry,
