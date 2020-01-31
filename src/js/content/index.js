0e425fa988b2fee9c856a39f893592b5a1bf6088
James Ivings <james@squarecat.io>
2020-01-31 16:40:22 +0800

    🐛 fix issue patching event listeners being called in wrong context

--- src/js/content/index.js
+++ src/js/content/index.js
@@ -43,12 +43,16 @@ function injectPatchScript() {
  * Subscription Score patch script
  * More info: https://github.com/subscriptionscore/extension
 */
-Element.prototype._addEventListener = Element.prototype.addEventListener;  
-Element.prototype.addEventListener = function(eventName, fn, ...args) {    
-  if (eventName === 'submit') {    
-    this._onsubmit = fn;      
-  } else {
-    return Element.prototype._addEventEventListener(eventName, fn, ...args);
+EventTarget.prototype._addEventListener = EventTarget.prototype.addEventListener;  
+EventTarget.prototype.addEventListener = function(type, listener, ...args) {       
+  if (type === 'submit') {
+    if (typeof listener === 'function') {
+      this._onsubmit = listener.bind(this);
+    } else if (typeof listener.handleEvent === 'function') {
+      this._onsubmit = listener.handleEvent.bind(this);
+    }
+  } else {    
+    return EventTarget.prototype._addEventListener.apply(this, [type, listener, ...args]);
   }
 };`;
   return awaitDomLoaded.then(() => {
