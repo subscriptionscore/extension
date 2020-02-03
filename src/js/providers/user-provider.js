25eadbb9e60d5babaa082cdadc869cf2623ddb7e
Danielle Johnson <dinkydani@gmail.com>
2020-02-03 14:56:25 +0800

    :bug: updating preferences throwing uncaught promise when user is not authorized

--- src/js/providers/user-provider.js
+++ src/js/providers/user-provider.js
@@ -66,8 +66,14 @@ const UserProvider = ({ children }) => {
   }, [licenceKey, setStorage, state.initialized, storage.licenceKey]);
 
   const updateStores = useCallback(async () => {
-    await updateUserPreferences(state.user.preferences);
-    await setStorage({ preferences: state.user.preferences });
+    try {
+      await updateUserPreferences(state.user.preferences);
+      await setStorage({ preferences: state.user.preferences });
+    } catch (err) {
+      // we dont need to know if these failed but this stops the extension
+      // from throwing an uncaught promise error
+      return true;
+    }
   }, [setStorage, state.user.preferences]);
 
   useEffect(() => {
